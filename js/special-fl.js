54f51dd87cd9113d16359db37a2a2675ba21c7b0
Sittipong HAUS <me@sittipong.com>
2017-08-27 03:26:47 +0700

    minify file && add animation

--- js/special-fl.js
+++ js/special-fl.js
@@ -2,7 +2,17 @@
   $('.fimg').each(function() {
     var attr = $(this).attr('data-image-src')
     if (typeof attr !== typeof undefined && attr !== false) {
-        $(this).css('background-image', 'url('+attr+')')
+      $(this).css('background-image', 'url('+attr+')')
+    }
+  })
+  $('.scroll-top').on('click', function () {
+    $('html, body').stop().animate({ scrollTop: 0 }, 2000, 'easeInOutExpo')
+    return false
+  })
+  $('a').on('click', function () {
+    if ( $(this).attr('href').indexOf('#') > -1 ) {
+      $('html, body').stop().animate({ scrollTop: $('#'+$(this).attr('href').replace('#','')).offset().top }, 2000, 'easeInOutExpo');
+      return false
     }
   })
 }()
