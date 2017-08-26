7003e90ecf2848f5983d45185631b4b3300ef14b
Sittipong HAUS <me@sittipong.com>
2017-08-26 10:27:18 +0700

    almost done

---
+++ js/special-fl.js
@@ -0,0 +1,8 @@
+!function(){
+  $('.fimg').each(function() {
+    var attr = $(this).attr('data-image-src')
+    if (typeof attr !== typeof undefined && attr !== false) {
+        $(this).css('background-image', 'url('+attr+')')
+    }
+  })
+}()
